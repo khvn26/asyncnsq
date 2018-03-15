1a7fc8cc0a298e033628d16dab40a2df078c2056
Kim Gustyr <gustyr@game-insight.com>
2018-03-15 18:50:57 +0300

    tests: fix tests importing common code, better consumer tests

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -19,50 +19,44 @@ class NsqTest(BaseTest):
         super().setUp()
 
     async def aioSetUp(self):
-        nsq = await create_nsq(host=self.host, port=self.port,
-                               heartbeat_interval=30000,
-                               feature_negotiation=True,
-                               tls_v1=True,
-                               snappy=False,
-                               deflate=False,
-                               deflate_level=0,
-                               loop=self.loop)
-
-        for i in range(1, self.total_test_msgs + 1):
-            await nsq.pub(b'foo', 'xxx:{}'.format(i).encode('utf-8'))
-
-        await asyncio.sleep(0.1, loop=self.loop)
-
+        self.producer = Nsqd(self.host, self.port+1, loop=self.loop)
         self.consumer = NsqConsumer(nsqd_tcp_addresses=[(self.host,
                                                          self.port)],
                                     max_in_flight=self.max_in_flight,
                                     loop=self.loop)
 
+        for i in range(1, self.total_test_msgs + 1):
+            await self.producer.pub(self.topic, 'xxx:{}'.format(i))
+
+        await asyncio.sleep(0.1, loop=self.loop)
+
         await self.consumer.connect()
         await self.consumer.subscribe('foo', 'bar')
 
     async def aioTearDown(self):
-        conn = Nsqd(self.host, self.port+1, loop=self.loop)
+        await self.consumer.close()
         try:
-            await conn.delete_topic(self.topic)
+            await self.producer.delete_topic(self.topic)
         except NotFoundError:
             pass
-        await conn.close()
+        finally:
+            await self.producer.close()
 
     @run_until_complete
     async def test_consumer_waiters(self):
         msgs = []
 
-        for i, waiter in enumerate(self.consumer.wait_messages(), 1):
-            if i == self.total_test_msgs:
-                break
-
+        for counter, waiter in enumerate(self.consumer.wait_messages(), 1):
             if self.consumer.is_starved():
                 await asyncio.gather(*[m.fin() for m in msgs])
                 msgs = []
 
-            msg = await waiter
-            msgs.append(msg)
+            if counter <= self.total_test_msgs:
+                msg = await waiter
+                msgs.append(msg)
+                continue
+
+            break
 
 
     @run_until_complete
